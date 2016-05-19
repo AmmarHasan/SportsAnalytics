eae30c66bd24ed27a9cd6c7e936a199b83797d68
AmmarHasan <hasan_ammar94@hotmail.com>
2016-05-19 18:55:59 +0500

    Seperate Player by position and save as binary format RDS

--- R/seperatePlayersByPostion.R
+++ R/seperatePlayersByPostion.R
@@ -1,30 +1,27 @@
 library(dplyr)
 
 #Read dataset
-soccer<-read.csv("D://AMMAR//FYP//Soccer DataSets//Premier League 2011-12 Match by Match.csv")
+soccer<-readRDS("output//Clean_PL_2011_12_Match_By_Match.rds")
 
 # Filtering by Position.Id
-# "saveRDS" saves a single object in binary or ASCII RDS format
-# ASCII will be more efficient with version control systems like Git
-
 
 # Position.Id 1 means Goal Keeper
 soccer %>%
   filter(Position.Id==1) %>%
-  saveRDS("output//GoalKeeper.rds", ascii=TRUE)
+  saveRDS("output//GoalKeeper.rds" )
 
 # Position.Id 2 means Defender
 soccer %>%
   filter(Position.Id==2) %>%
-  saveRDS("output//Defender.rds", ascii=TRUE)
+  saveRDS("output//Defender.rds" )
 
 
 # Position.Id 4 means MidFielder
 soccer %>%
   filter(Position.Id==4) %>%
-  saveRDS("output//MidFielder.rds", ascii=TRUE)
+  saveRDS("output//MidFielder.rds" )
 
 # Position.Id 6 means Attacker
 soccer %>%
   filter(Position.Id==6) %>%
-  saveRDS("output//Attacker.rds", ascii=TRUE)
+  saveRDS("output//Attacker.rds" )
