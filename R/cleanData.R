ab04367b80d0e640deda5072b616fcad54a67f0e
AmmarHasan <hasan_ammar94@hotmail.com>
2016-05-19 18:57:42 +0500

    Select important attributes of PL11_12 manually

---
+++ R/cleanData.R
@@ -0,0 +1,18 @@
+library(dplyr) 
+#Read dataset
+soccer<-read.csv("data//Premier League 2011-12 Match by Match.csv",header =T,sep =',')
+
+
+soccer %>%
+  mutate(Player.Name = paste(Player.Surname , Player.Forename , sep=" ") )%>%
+  select_("Date","Position.Id","Player.ID","Player.Name","Team","Opposition","Venue",
+          "Time.Played","Goals","Shots.On.Target.inc.goals","Shots.Off.Target.inc.woodwork",
+          "Blocked.Shots","Total.Successful.Passes.All","Total.Unsuccessful.Passes.All","Assists",
+          "Key.Passes","Successful.Dribbles","Unsuccessful.Dribbles","Successful.Crosses.Right",
+          "Successful.Crosses.Left","Duels.won","Duels.lost","Tackles.Won","Tackles.Lost",
+          "Last.Man.Tackle","Total.Clearances","Blocks","Interceptions","Recoveries",
+          "Total.Fouls.Conceded","Total.Fouls.Won","Offsides","Red.Cards","Yellow.Cards",
+          "Goals.Conceded","Saves.Made","Catches","Punches","Drops","GK.Distribution",
+          "Clean.Sheets","Error.leading.to.Goal","Error.leading.to.Attempt","Dispossessed"
+          ,"Unsuccessful.Ball.Touch","Successful.Ball.Touch") %>%
+  saveRDS("output//Clean_PL_2011_12_Match_By_Match.rds")
